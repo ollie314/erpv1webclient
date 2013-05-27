a37c690a0b61b2516b10844246f938b75c08d95a
ollie314 <mehdi.lefebvre@gmail.com>
2013-05-27 12:42:59 +0200

    Add mousewheel dependency.

---
+++ assets/js/views/hub/modules/site/nls/filter.js
@@ -0,0 +1,11 @@
+define({
+    "root" : true,
+    "fr-fr": true
+});
+
+/*
+ * Ajouter dupliquer dans la liste. Clone editable [warn id]
+ * Ajouter colonne piece.
+ * Supprimer le sous ouvrage, concerne / client automatique [non editable]
+ * Ajouter le nom de l'ouvrier dans le champ de recherche
+ */
