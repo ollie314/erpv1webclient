634c75c68461911ee4499587ef561f377ebf5c85
ollie314 <mehdi.lefebvre@gmail.com>
2013-06-01 10:54:44 +0200

    Manage the behavior during the navigation.

--- assets/js/views/hub/modules/site/navigation/SiteManagerBottomBarView.js
+++ assets/js/views/hub/modules/site/navigation/SiteManagerBottomBarView.js
@@ -33,10 +33,18 @@ define([
                         }
                     },
                     'visible': {
-                        'hide': {enterState: 'hidden'}
+                        'hide': {
+                            enterState: 'hidden',
+                            triggers: 'site:nav:bottom:hide',
+                            callbacks: []
+                        }
                     },
                     'hidden': {
-                        'show': {enterState: 'visible'}
+                        'show': {
+                            enterState: 'visible',
+                            triggers: 'site:nav:bottom:show',
+                            callbacks: []
+                        }
                     }
                 },
                 doShow: function () {
@@ -80,6 +88,27 @@ define([
             },
             initialize: function () {
                 createStateMachine(this);
+            },
+            hide: function() {
+                this.trigger('hide');
+                mediator.publish('site:bottombar:hiding');
+            },
+            detach: function() {
+                // should remove element from the dom but keep it in the view stack
+                if(!erp.viewManager.has(Erp.ViewNames.SITE_MANAGER_BOTTOM_BAR)) {
+                    erp.viewManager.push(Erp.ViewNames.SITE_MANAGER_BOTTOM_BAR, this);
+                }
+                this.$el.html('');
+                mediator.publish('site:bottombar:detached');
+            },
+            attach: function() {
+                // attach the view to the DOM. It should look for view into the view stack
+                if(erp.viewManager.has(Erp.ViewNames.SITE_MANAGER_BOTTOM_BAR)) {
+                    var view = erp.viewManager.get(Erp.ViewNames.SITE_MANAGER_BOTTOM_BAR);
+                    view.render();
+                    mediator.publish('site:bottombar:attached');
+                }
+
             }
         });
     return BottomBarHubView;
