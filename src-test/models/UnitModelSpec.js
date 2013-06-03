e1ad5743435e89eaac231b51632c88a8c47fd895
ollie314 <mehdi.lefebvre@gmail.com>
2013-06-03 10:08:57 +0200

    cleaning for code quality.

--- src-test/models/UnitModelSpec.js
+++ src-test/models/UnitModelSpec.js
@@ -5,16 +5,24 @@
  * Time: 14:15
  * To change this template use File | Settings | File Templates.
  */
-describe('Unit', function() {
-    beforeEach(function() {
+/*global console*/
+/*global describe*/
+/*global beforeEach*/
+/*global it*/
+/*global expect*/
+/*global Unit*/
+describe('Unit', function () {
+    'use strict';
+    var unit;
+    beforeEach(function () {
         unit = new Unit();
     });
 
-    it("should id equals to 0", function() {
+    it("should id equals to 0", function () {
         expect(unit.id).toEqual(0);
     });
 
-    it("Should name correctly formatted", function() {
+    it("Should name correctly formatted", function () {
         unit.firstname = "Mehdi";
         unit.lastname = "Lefebvre";
         console.log("OK:");
